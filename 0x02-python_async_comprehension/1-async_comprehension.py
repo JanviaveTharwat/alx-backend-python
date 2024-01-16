aba3abbcae0ddd4c4d9d0d7dee41a9a3e9a25888
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2024-01-16 19:55:17 +0200

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" a python module to returns 10 random numbers using async comprehension"""
+'''task 1'''
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
